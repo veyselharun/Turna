882b50850b00f92a7935da21fc2671a41a776dc3
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2022-03-13 20:04:01 +0300

    Add function call test and reorganize directory layout

---
+++ test_data/function_call_test/function_call_test.c
@@ -0,0 +1,18 @@
+
+int calculate(int a, int b) {
+	return (a + b);
+}
+
+
+int main() {
+	int c;
+    int d;
+    int e;
+
+	c = calculate(5,7);
+    e = 2;
+    e = e * c;
+    d = calculate(3,4);
+    e = e + d;
+}
+
